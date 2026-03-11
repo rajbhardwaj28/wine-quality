5ddc27a8666cb2af4d8471bcc89418a8df736c99
rajbhardwaj28 <rajraman8595@gmail.com>
2026-03-11 20:30:07 +0530

    Update model file name in deployment script

--- wine_quality_deployment.py
+++ wine_quality_deployment.py
@@ -11,7 +11,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("wine_quality_model.pkl")
+model = joblib.load("wine_quality_prediction.pkl")
 
 st.title("Wine Quality Merged System")
 
